78f4e32e530f4847f978ad3911235dfd8e8192cb
alikhanmywebsite <66816783+alikhanmywebsite@users.noreply.github.com>
2020-08-07 02:05:19 -0700

    Quizjava
    Vishnu Shahabaj created

---
+++ question.js
@@ -0,0 +1,113 @@
+var questions=[{
+  "question":"Which of the following will directly stop the execution of a Thread? ",
+  "option1":"wait()",
+  "option2" :"notify()",
+  "option3"	:"notifyall()",
+  "option4"	:"exits synchronized code",
+  "answer":"1"
+},{
+	"question":"Which arithmetic operations can result in the throwing of an ArithmeticException?",
+"option1"	:"/ , %",
+"option2"	:"* , +",
+	"option3":"! , -",
+	"option4":">>, <<",
+	"answer":"1"
+	
+},{
+"question":"Which data type is used to create a variable that should store text?",
+	"option1":"string",
+   "option2" :"String",
+   "option3" :"myString",
+	"option4":"txt",
+	"answer":"2"
+},{
+	"question":"What allows the programmer to destroy an object x?",
+	"option1":"x.delete()",	
+	"option2":"x.finalize()",
+	"option3":"Runtime.getRuntime().gc()",	
+	"option4":"Only the garbage collection system can destroy an object",
+	"answer":"4"
+},{
+	"question":"Which statement is true? ",
+
+	"option1":"All objects that are eligible for garbage collection will be garbage collected by the garbage collector",
+	"option2":"Objects with at least one reference will never be garbage collected.",
+	"option3":"Objects from a class with the finalize() method overridden will never be garbage collected.",
+	"option4":"Objects instantiated within anonymous inner classes are placed in the garbage collectible heap",
+"answer":"4"
+},{   
+     "question":"Which is true about an anonymous inner class? ",
+"option1"	:"It can extend exactly one class and implement exactly one interface.",
+"option2"	:"It can extend exactly one class and can implement multiple interfaces.",
+  "option3" :"It can extend exactly one class or implement exactly one interface.",
+   "option4":"It can implement multiple interfaces regardless of whether it also extends a class.",
+   "answer":"4"
+},{
+"question"	:"What is the name of the method used to start a thread execution? ",
+		"option1":"init();",
+    "option2":"start();",
+	"option3":"run();",
+	"option4":"resume();",
+	"answer":"2"
+},{
+	"question":"Which of these can be used to fully abstract a class from its implementation?",
+	"option1":"Objects",
+	"option2":"Packages",
+	"option3":"Interfaces.",
+	"option4":"None of the Mentioned",
+
+	"answer":"3"
+	
+},{
+	"question":"Which of the following stops execution of a thread? ",
+ "option1":"Calling SetPriority() method on a Thread object",
+ "option2":"Calling notify() method on an object",
+ "option3":"Calling wait() method on an object",
+"option4":" Calling read() method on an InputStream object",
+"answer":"2"
+	
+},{
+"question"	:"How many arguments can be passed to main()?",
+ "option1":"Infinite",
+ "option2":"Only 1",
+ "option3":"System Dependent",
+ "option4":"None of the mentioned",
+ "answer":"1"
+},{
+"question":"Which of the following is a correct constructor for thread? ",
+ "option1":"Thread(Runnable a, String str)",
+ "option2":"Thread(int priority)",
+ "option3":"Thread(Runnable a, int priority)",
+"option4" :"Thread(Runnable a, ThreadGroup t)",
+"answer":"1"
+},{
+	
+"question":"Which of these data types is used to store command line arguments? ",
+"option1":"Array",
+ "option2":"Stack",
+ "option3":"String",
+ "option4":"Integer",
+"answer":"3"
+},{
+	"question":"When Overloading does not occur? ",
+ "option1":"More than one method with same name but different method signature and different number or type of parameters",
+"option2" :"More than one method with same name, same signature but different number of signature",
+ "option3":"More than one method with same name, same signature, same number of parameters but different type",
+ "option4":"More than one method with same name, same number of parameters and type but different signature",
+"answer":"4"
+	
+},{	
+"question":"Which of these keywords is used by a class to use an interface defined previously? ",
+ "option1":"import ",
+ "option2":"Import",
+ "option3":"implements",
+ "option4":"Implements",
+ "answer":"3"
+},{
+"question":"Which of the following classes can catch all exceptions which cannot be caught? ",
+ "option1":"RuntimeException ",
+"option2" :"Error",
+ "option3":"Exception",
+ "option4":"ParentException",
+"answer":"2"	
+}]
